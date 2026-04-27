d5240739218a4e4bcfa1b4a8d06d04f1b98e5252
Olivia <omparke1@asu.edu>
2026-04-26 22:10:56 -0700

    trying to get javascript to function for shopping cart

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ toggle.addEventListener("click", (e) => {
     e.preventDefault(); 
     document.body.classList.toggle("dark-mode");
 });
+
+
+// shop section pricing
+
+const buttons = document.querySelectorAll(".plants button");
+const cartBtn = document.getElementById("cart-btn");
+
+let total = 0;
+let count = 0;
+
+buttons.forEach(button => {
+    button.addEventListener("click", () =>{
+const li = document.createElement("li");
+li.textContent = `${name} - $${price}`;
+cartItems.appendChild(li);
+
+total += price;
+totalPriceEl.textContent = total.toFixed(2);
+
+// cart count
+count++;
+cartBtn.textContent = `Cart (${count})`;
+    });
+});
