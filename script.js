4851e95e85a929943856a7edf45c11943988737f
Olivia <omparke1@asu.edu>
2026-05-01 15:50:32 -0700

    Added additional comments to explain javascript more thoroughly

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ toggle.addEventListener("click", (e) => {
 
 // form errors
 
+//calling elements
 const form = document.querySelector("form");
 
 const nameInput = document.getElementById("fullName");
@@ -25,20 +26,25 @@ const emailError = document.getElementById("email-error");
 const commentError = document.getElementById("comment-error");
 const messageEl = document.getElementById("form-message");
 
+//regex for phone/email checking
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const phoneRegex = /^\d{10}$/;
 
+
+//when form is submitted check for these elements
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     let isValid = true;
 
+//form input reset
     nameError.textContent = "";
     phoneError.textContent = "";
     emailError.textContent = "";
     commentError.textContent = "";
     messageEl.textContent = "";
 
+//if these elements aren't seen show these errors
 if (nameInput.value.trim() === ""){
     nameError.textContent = "Please enter your full name.";
     isValid = false;
@@ -79,6 +85,7 @@ if (!selectedContact){
     }
 }
 
+//if all elements are valid
 if (isValid){
 const customer = {
     name: nameInput.value.trim(),
@@ -88,8 +95,10 @@ const customer = {
     preferredContact: selectedContact.value
 };
 
+//valid form output
 messageEl.textContent = `Thank you, ${customer.name}! We will contact you via ${customer.preferredContact}. Your comment: "${customer.comment}"`;
 
+//reset form
 form.reset();
 }
 });
@@ -97,46 +106,53 @@ form.reset();
 
 // shop section pricing
 
+//calling elements
 const buttons = document.querySelectorAll(".plants button");
 const cartItems = document.getElementById("cart-items");
 const totalPriceEl = document.getElementById("total-price");
 const cartBtn = document.getElementById("cart-btn");
 
+//total reset
 let total = 0;
 let count = 0;
 
+//when item is added to cart
 buttons.forEach(button => {
     button.addEventListener("click", () =>{
+
+//calling additional elements
 const plant = button.closest(".plants");
 const name = plant.querySelector("h3").textContent;
 const price = parseFloat(plant.dataset.price);
 const img = plant.querySelector("img");
 
 const li = document.createElement("li");
-
 const imgClone = img.cloneNode(true);
 imgClone.style.width = "50px";
 
 const text = document.createElement("span");
 
+//add item name/price to cart
 text.textContent = `${name} - $${price.toFixed(2)}`;
 
+//add image of item to cart
 li.appendChild(imgClone);
 li.appendChild(text);
 cartItems.appendChild(li);
 
+//add price totals
 total += price;
 totalPriceEl.textContent = total.toFixed(2);
 
-// cart count
+//cart count output (total items in cart)
 count++;
 cartBtn.textContent = `Cart (${count})`;
 
-// tax count
-
+//calling additional elements for tax count
 const taxPriceEl = document.getElementById("tax-price");
 const taxRate = 0.08;
 
+// tax count calculating
 const tax = total * taxRate;
 taxPriceEl.textContent = tax.toFixed(2);
 
@@ -153,19 +169,22 @@ const taxRate = 0.08;
 const taxPriceEl = document.getElementById("tax-price");
 const finalPriceEl = document.getElementById("final-price");
 
-
+//cart button when clicked
 checkoutBtn.addEventListener("click", () => {
 
+    //if cart is empty when clicked
     if (count === 0){
         alert("Your cart is empty. Please add items before checking out.");
         return;
     }
 
+    //if cart is full when clicked, calculate overall total and output text showing order submission
     const tax = total * taxRate;
     const finalTotal = total + tax;
 
     alert(`Thank you for your order!\nTotal: $${finalTotal.toFixed(2)}`);
 
+    //cart resets
    document.querySelectorAll("#cart-items li").forEach(item => item.remove());
 
     total = 0;
